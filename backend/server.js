13a462ad0761cce3fadbfc8b83988745d630408c
Avinashraj21 <avinashselvi214@gmail.com>
2024-09-30 22:06:00 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const newsRoutes = require('./routes/news');
 app.use('/api/news', newsRoutes);
 app.use('/api/subscription', subscriptionRoutes.router);
 
-mongoose.connect('mongodb://localhost/news-app', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://avinashselvi214:avinash@newsappcluster0.8vvss.mongodb.net/?retryWrites=true&w=majority&appName=newsappCluster0', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
